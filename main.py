86f3cd8fc6511aecf6fbd434ae0ce780205046af
Joshua King <joshuajking84@gmail.com>
2023-01-08 23:29:40 -0800

    removed unused variables and print statments

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import time
 from login_to_characters import login_to_character
 
 
-cycles = 70
-count = 1
 while True:
     start_time = time.perf_counter()
     login_to_character()
@@ -13,5 +11,3 @@ while True:
     elapsed_time = end_time - start_time
     logger.info(f"Time Elapsed Cycles: {elapsed_time:.2f} seconds")
     print(f"Time Elapsed: {elapsed_time:.2f} seconds")
-    print(f"cycled characters {count}")
-    count += 1
