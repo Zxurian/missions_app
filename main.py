7f5ca5497d6720ec702ce331f6d03a5a3916d3f1
Zxurian <zxurian@gmail.com>
2023-01-14 03:06:41 -0500

    removing unnecessary code

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from login_to_characters import automate_character
 
 
 def main():
-    while True:
-        start_time = time.perf_counter()
-        automate_character()
-        end_time = time.perf_counter()
-        elapsed_time = end_time - start_time
-        logger.info(f"Time Elapsed Cycles: {elapsed_time:.2f} seconds")
-        print(f"Time Elapsed: {elapsed_time:.2f} seconds")
+    start_time = time.perf_counter()
+    automate_character()
+    end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
+    logger.info(f"Time Elapsed Cycles: {elapsed_time:.2f} seconds")
+    print(f"Time Elapsed: {elapsed_time:.2f} seconds")
 
 
 if __name__ == "__main__":
