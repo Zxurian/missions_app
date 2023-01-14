835abe5e1e814c89c10bf0eded6de62b9c337220
Joshua King <joshuajking84@gmail.com>
2023-01-13 19:01:28 -0800

    added if __name__ to main

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from log import logger
 from login_to_characters import login_to_character
 
 
-while True:
-    start_time = time.perf_counter()
-    login_to_character()
-    end_time = time.perf_counter()
-    elapsed_time = end_time - start_time
-    logger.info(f"Time Elapsed Cycles: {elapsed_time:.2f} seconds")
-    print(f"Time Elapsed: {elapsed_time:.2f} seconds")
+def main():
+    while True:
+        start_time = time.perf_counter()
+        login_to_character()
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        logger.info(f"Time Elapsed Cycles: {elapsed_time:.2f} seconds")
+        print(f"Time Elapsed: {elapsed_time:.2f} seconds")
+
+
+if __name__ == "__main__":
+    main()
