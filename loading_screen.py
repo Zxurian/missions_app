56f59e0127e8690ae60f628de8a0c60887686f8f
Joshua King <joshuajking84@gmail.com>
2023-01-13 19:00:40 -0800

    added some logic to at_loading_screen

--- loading_screen.py
+++ loading_screen.py
@@ -9,5 +9,8 @@ def at_loading_screen():
     # check to see if the login has completed and we're on the loading screen
     at_loading_screen = check_for_screen("Loading Complete", ip.loading_complete, 0.6)
     logger.info("Loading Complete")
-    if at_loading_screen == None:
+    if at_loading_screen != None:
+        return True
+    else:
         logger.info(f" Not at {at_loading_screen}")
+        return False
