242cdbdfdc733528228f2df1c5fdef7769b359c5
Joshua King <joshuajking84@gmail.com>
2023-01-15 10:22:37 -0800

    clairifed logger msg

--- loading_screen.py
+++ loading_screen.py
@@ -1,4 +1,3 @@
-
 from log import logger
 import img_page as ip
 from verify_img import check_for_screen
@@ -7,11 +6,10 @@ from verify_img import check_for_screen
 def at_loading_screen():
 
     # check to see if the login has completed and we're on the loading screen
-    at_loading_screen = check_for_screen(
-        "Loading Complete", ip.loading_complete, 0.6)
+    at_loading_screen = check_for_screen("Loading Complete", ip.loading_complete, 0.6)
     logger.info("Loading Complete")
     if at_loading_screen != None:
         return True
     else:
-        logger.info(f" Not at {at_loading_screen}")
+        logger.info(f" Not at at_loading_screen")
         return False
