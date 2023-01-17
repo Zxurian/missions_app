3b58b432fd655afeac510fdd3c3fe6464622b475
Joshua King <joshuajking84@gmail.com>
2023-01-16 20:22:46 -0800

    added while loop to check login

--- login_credentials.py
+++ login_credentials.py
@@ -14,36 +14,45 @@ ahk = AHK()
 def login_with_credentials(username, password):
 
     print(username)
-    # print(password)
 
-    pydir.press("tab")
-    pydir.press("backspace")
-    pydir.press("tab")
-    pydir.press("backspace")
-    needle_email = pygui.locateCenterOnScreen(
-        ip.needle_email_login, confidence=0.8, minSearchTime=1
-    )
-    pygui.click(needle_email, clicks=2)
-    sleep(1)
-    # pygui.write(username)
-    ahk.send_input(username)
-    needle_pwd = pygui.locateCenterOnScreen(
-        ip.needle_pwd_login, confidence=0.8, minSearchTime=1
-    )
-    sleep(1)
-    pygui.click(needle_pwd, clicks=2)
-    sleep(1)
-    # pygui.write(password)
-    # sleep(1)
-    ahk.send_input(password)
-    pydir.press("enter")
+    attempts = 3
+    count = 0
+    while count < attempts:
+        pydir.press("tab")
+        pydir.press("backspace")
+        pydir.press("tab")
+        pydir.press("backspace")
+        needle_email = pygui.locateCenterOnScreen(
+            ip.needle_email_login, confidence=0.8, minSearchTime=1
+        )
+        pygui.click(needle_email, clicks=2)
+        sleep(1)
+        # pygui.write(username)
+        ahk.send_input(username)
+        needle_pwd = pygui.locateCenterOnScreen(
+            ip.needle_pwd_login, confidence=0.8, minSearchTime=1
+        )
+        sleep(1)
+        pygui.click(needle_pwd, clicks=2)
+        sleep(1)
+        # pygui.write(password)
+        # sleep(1)
 
-    gametime_error = check_for_screen(
-        "gametime_error", ip.needle_gametime_error, 0.9, True
-    )
+        ahk.send_input(password)
 
-    if gametime_error:
-        return True
-    # TODO would like to have a notification sent when gametime_error found
-    else:
-        return False
+        email = pygui.locateOnScreen(
+            ip.email_login_login, confidence=0.8, minSearchTime=1
+        )
+        if email:
+            continue
+        else:
+            pydir.press("enter")
+            gametime_error = check_for_screen(
+                "gametime_error", ip.needle_gametime_error, 0.9, True
+            )
+
+            if gametime_error:
+                return True
+            # TODO would like to have a notification sent when gametime_error found
+            else:
+                return False
