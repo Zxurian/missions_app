53e8aec75f212a2d08a981e2adf1b4b799410228
Zxurian <zxurian@gmail.com>
2023-01-19 17:43:24 -0500

    adding exceptions

--- utilities.py
+++ utilities.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import time
 
 
@@ -7,17 +8,46 @@ class Stopwatch:
     total_time = None
 
     def start(self) -> Stopwatch:
+        if self.stop_time is not None:
+            raise StopwatchException(StopwatchException.STOPWATCH_ALREADY_STOPPED)
+
+        if self.start_time is not None:
+            raise StopwatchException(StopwatchException.STOPWATCH_ALREADY_STARTED)
+
         self.start_time = time.perf_counter()
         return self
 
     def stop(self) -> Stopwatch:
+        if self.start_time is None:
+            raise StopwatchException(StopwatchException.STOPWATCH_NOT_STARTED)
+
+        if self.stop_time is not None:
+            raise StopwatchException(StopwatchException.STOPWATCH_ALREADY_STOPPED)
+
         self.stop_time = time.perf_counter()
         self.total_time = self.stop_time - self.start_time
         return self
 
     def get_total_time(self) -> float:
+        if self.start_time is None:
+            raise StopwatchException(StopwatchException.STOPWATCH_NOT_STARTED)
+        if self.stop_time is None:
+            raise StopwatchException(StopwatchException.STOPWATCH_NOT_STOPPED)
+
         return self.total_time
 
     def get_elapsed(self) -> float:
-        return time.perf_counter() - self.start_time
+        if self.start_time is None:
+            raise StopwatchException(StopwatchException.STOPWATCH_NOT_STARTED)
 
+        if self.stop_time is None:
+            return time.perf_counter() - self.start_time
+        else:
+            return self.total_time
+
+
+class StopwatchException(Exception):
+    STOPWATCH_NOT_STARTED = 1
+    STOPWATCH_ALREADY_STARTED = 2
+    STOPWATCH_NOT_STOPPED = 3
+    STOPWATCH_ALREADY_STOPPED = 4
